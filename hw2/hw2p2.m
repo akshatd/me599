e50ff03af5cb2ac8c02d9475850881d0f11ce122
Akshat Dubey <akshatdubey@outlook.com>
2025-02-13 15:10:58 -0500

    display equation of hyperplane

--- hw2/hw2p2.m
+++ hw2/hw2p2.m
@@ -13,8 +13,9 @@ Z = [ones(1, size(X_1, 2)), -ones(1, size(X_2, 2))];
 
 %% P2a Perceptron
 W_hat_0 = rand(3, 1); % random weights
-options = optimoptions('fmincon', 'Display', 'iter', 'SpecifyObjectiveGradient', true);
+options = optimoptions('fmincon', 'SpecifyObjectiveGradient', true);
 W_opt = fmincon(@(W) perceptronCostWGrad(X, Z, W), W_hat_0, [],[],[],[],[],[],[], options); % optimize weights
+fprintf('Equation of Perceptron hyperplane: x2 = %fx1 + %f\n', -W_opt(1)/W_opt(2), -W_opt(3)/W_opt(2));
 
 % plot
 x1_min = min(X(1, :)); x1_max = max(X(1, :));
@@ -26,6 +27,7 @@ scatter(X_1(1, :), X_1(2, :), 'filled', 'DisplayName', 'Class 1');
 hold on;
 scatter(X_2(1, :), X_2(2, :), 'filled', 'DisplayName', 'Class 2');
 plot(x1, x2, 'k', 'LineWidth', 1, 'DisplayName', 'Separating hyperplane(Perceptron)');
+xlabel('x1'); ylabel('x2');
 title('HW2 P2a: Separating hyperplane using Perceptron');
 legend('Location', 'best');
 
@@ -36,6 +38,7 @@ H = eye(3);
 f = zeros(3, 1);
 A = -Z'.*X'; b = -ones(size(Z));
 W_opt = quadprog(H, f, A, b); % optimize weights
+fprintf('Equation of SVM hyperplane: x2 = %fx1 + %f\n', -W_opt(1)/W_opt(2), -W_opt(3)/W_opt(2));
 
 % plot
 x2 = lineFromWights(W_opt, x1);
@@ -45,6 +48,7 @@ scatter(X_1(1, :), X_1(2, :), 'filled', 'DisplayName', 'Class 1');
 hold on;
 scatter(X_2(1, :), X_2(2, :), 'filled', 'DisplayName', 'Class 2');
 plot(x1, x2, 'k', 'LineWidth', 1, 'DisplayName', 'Separating hyperplane(SVM)');
+xlabel('x1'); ylabel('x2');
 title('HW2 P2b: Separating hyperplane using SVM');
 legend('Location', 'best');
 
